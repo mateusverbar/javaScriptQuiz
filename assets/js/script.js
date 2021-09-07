17748070a19fda96a823e11b007d6fbc0e44814c
Matthew Barros <matthewbarros@Matthews-MacBook-Pro.local>
2021-09-06 20:48:11 -0500

    replace page content on start click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,30 @@
+var mainEl = document.querySelector("#main");
+var startPage = document.createElement("section");
+startPage.className = "start-page";
+
+var startButton = document.createElement("button");
+startButton.setAttribute("type","button");
+startButton.setAttribute("id","startbtn");
+startButton.className = "btn";
+startButton.innerHTML = "Start Quiz";
+
+startPage.innerHTML = "<h1>Coding Quiz Challenge</h1><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, molestias culpa placeat necessitatibus, voluptatem error, blanditiis eaque corporis voluptatum odit quisquam ex. Deleniti, eum nam nobis magnam provident itaque molestiae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis autem culpa provident amet. Modi totam quis aspernatur facilis vitae, voluptatum facere cum nulla, dignissimos possimus animi illo eveniet explicabo aut!</p>";
+
 var loadStartPage = function() {
-    var startPage = document.createElement("section");
-    startPage.className = "start-page";
-
-    var startButton = document.createElement("button");
-    startButton.setAttribute("type","button");
-    startButton.className = "btn";
-    startButton.innerHTML = "Start Quiz";
-
-    startPage.innerHTML = "<h1>Coding Quiz Challenge</h1><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, molestias culpa placeat necessitatibus, voluptatem error, blanditiis eaque corporis voluptatum odit quisquam ex. Deleniti, eum nam nobis magnam provident itaque molestiae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis autem culpa provident amet. Modi totam quis aspernatur facilis vitae, voluptatum facere cum nulla, dignissimos possimus animi illo eveniet explicabo aut!</p>";
-
     mainEl.appendChild(startPage);
     startPage.appendChild(startButton);
-    
 }
 
-var mainEl = document.querySelector("#main");
+var startQuiz = function() {
+var elmnt = document.createElement("section");
+elmnt.innerHTML = "<h1>It worked!</h1>";
+elmnt.setAttribute("id","testing");
+
+mainEl = document.querySelector("#main");
+mainEl.removeChild(startPage);
+mainEl.appendChild(elmnt);
+}
+
 document.addEventListener("load",loadStartPage());
 
+startButton.onclick = function() {startQuiz()};
