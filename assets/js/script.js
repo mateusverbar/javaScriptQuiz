68c59cbc0de62a6ed61618995bf9c69889c49449
Matthew Barros <matthewbarros@Matthews-MacBook-Pro.local>
2021-09-07 15:14:35 -0500

    got questions to load sequentially on answer button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,3 @@
-//var questionIdCounter = 0;
-
-var mainEl = document.querySelector("#main");//where all the action happens
-
-var startPage = document.createElement("section");
-startPage.className = "start-page";
-
-var startButton = document.createElement("button");
-startButton.setAttribute("type","button");
-startButton.setAttribute("id","startbtn");
-startButton.className = "btn";
-startButton.innerHTML = "Start Quiz";
-
-startPage.innerHTML = "<h1>Coding Quiz Challenge</h1><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, molestias culpa placeat necessitatibus, voluptatem error, blanditiis eaque corporis voluptatum odit quisquam ex. Deleniti, eum nam nobis magnam provident itaque molestiae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis autem culpa provident amet. Modi totam quis aspernatur facilis vitae, voluptatum facere cum nulla, dignissimos possimus animi illo eveniet explicabo aut!</p>";
-
-var loadStartPage = function() {
-    mainEl.appendChild(startPage);
-    startPage.appendChild(startButton);
-}
-
-var startQuiz = function() {
-var quizQuestion = document.createElement("section");
-quizQuestion.className = "live-quiz";
-quizQuestionContainer = document.createElement("ul");
-quizQuestionContainer.className = "question-container";
-
-for (var i = 0; i < questions.length; i++) {
-    quizQuestion.innerHTML = "<h1>" + questions[i].question + "</h1>";
-    
-    quizQuestionContainer.innerHTML = "<li><button class='answer-choice' type='button'>1. " + questions[i].answerOne + "</button></li><li><button class='answer-choice' type='button'>2. " + questions[i].answerTwo + "</button></li><li><button class='answer-choice' type='button'>3. " + questions[i].answerThree + "</button></li><li><button class='answer-choice' type='button'>3. " + questions[i].answerFour + "</button></li>";
-
-    debugger;
-    quizQuestion.appendChild(quizQuestionContainer);
-    if (document.querySelector(".start-page") === startPage) {
-        mainEl.replaceChild(quizQuestion,startPage);
-    }
-    else if (document.querySelector(".start-page" === !startPage)) {
-        mainEl.replaceChild(quizQuestion,quizQuestion); //wasn't sure this would work! But apparently it does
-    }
-}
-
-//Ok, so the for-loop does cycle through the questions and load them right in the debugger. The problem is that the program keeps going until the last object in the array, and for the user, only displays that last question on "start." So how do we make the program stop and wait for an input after each iteration of the for-loop? This is crucial for the quiz, as each question loads, the for-loop stops, waits for an input (that we can do something with - like increment a score), before proceeding to the next iteration
-
-//quizQuestion.appendChild(quizQuestionContainer);
-
-//mainEl = document.querySelector("#main");
-//mainEl.removeChild(startPage);
-//mainEl.appendChild(elmnt);
-//quizQuestion.appendChild(quizQuestionContainer);
-//mainEl.replaceChild(quizQuestion,startPage);
-}
-
 var questions = [{
     question: "How much script can javaScript script if javaScript could script script?",
     answerOne: "So much Script!",
@@ -72,6 +20,70 @@ var questions = [{
     answerFour: "Blorps."
 }];
 
+//var questionIdCounter = 0;
+
+var mainEl = document.querySelector("#main");//where all the action happens
+
+var startPage = document.createElement("section");
+startPage.className = "start-page";
+
+var startButton = document.createElement("button");
+startButton.setAttribute("type","button");
+startButton.setAttribute("id","startbtn");
+startButton.className = "btn";
+startButton.innerHTML = "Start Quiz";
+
+startPage.innerHTML = "<h1>Coding Quiz Challenge</h1><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, molestias culpa placeat necessitatibus, voluptatem error, blanditiis eaque corporis voluptatum odit quisquam ex. Deleniti, eum nam nobis magnam provident itaque molestiae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis autem culpa provident amet. Modi totam quis aspernatur facilis vitae, voluptatum facere cum nulla, dignissimos possimus animi illo eveniet explicabo aut!</p>";
+
+var i = 0;
+
+    var loadStartPage = function() {
+        mainEl.appendChild(startPage);
+        startPage.appendChild(startButton);
+    }
+    
+    var takeQuiz = function() {
+        
+        if (i < questions.length) {
+        //replace child of MainEl with quizQuestion as populated by questions[i]
+        //mainEl.removeChild(startPage);
+
+        var quizQuestion = document.createElement("section");
+        quizQuestion.className = "live-quiz";
+        quizQuestionContainer = document.createElement("ul");
+        quizQuestionContainer.className = "question-container";
+        debugger;
+        quizQuestion.innerHTML = "<h1>" + questions[i].question + "</h1>";
+        quizQuestionContainer.innerHTML = "<li><button class='answer-choice' type='button' id='inc1'>1. " + questions[i].answerOne + "</button></li><li><button class='answer-choice' type='button' id='inc2'>2. " + questions[i].answerTwo + "</button></li><li><button class='answer-choice' type='button' id='cor'>3. " + questions[i].answerThree + "</button></li><li><button class='answer-choice' type='button' id='inc3'>4. " + questions[i].answerFour + "</button></li>";
+        
+        quizQuestion.appendChild(quizQuestionContainer);
+
+        mainEl.innerHTML = "";
+        mainEl.appendChild(quizQuestion);
+
+        i++;
+
+        var ans1 = document.getElementById('inc1');
+        var ans2 = document.getElementById('inc2');
+        var ans3 = document.getElementById('cor');
+        var ans4 = document.getElementById('inc3');
+        console.log(ans1,ans2,ans3,ans4);
+
+        ans1.onclick = function() {takeQuiz()};
+        ans2.onclick = function() {takeQuiz()};
+        ans3.onclick = function() {takeQuiz()};
+        ans4.onclick = function() {takeQuiz()};
+        }
+        
+    }
+
+
+    //var endQuiz = function() {...}
+
 document.addEventListener("load",loadStartPage());
 
-startButton.onclick = function() {startQuiz()};
+startButton.onclick = function() {takeQuiz()};
+
+//answerBtn.onclick = function() {takeQuiz()};
+
+//document.getElementsByClassName('answer-choice').addEventListener("click",takeQuiz());
