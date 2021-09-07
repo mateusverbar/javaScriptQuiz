3dfb8582136d14811e9ea136a66a3ed3ff15f0c2
Matthew Barros <matthewbarros@Matthews-MacBook-Pro.local>
2021-09-07 01:10:15 -0500

    debugging sequential question load

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 //var questionIdCounter = 0;
 
 var mainEl = document.querySelector("#main");
+
 var startPage = document.createElement("section");
 startPage.className = "start-page";
 
@@ -23,14 +24,23 @@ quizQuestion.className = "live-quiz";
 quizQuestionContainer = document.createElement("ul");
 quizQuestionContainer.className = "question-container";
 
-for (let i = 0; i < questions.length; i++) {
-quizQuestionContainer.innerHTML = "<li><button class='answer-choice' type='button'>1. " + questions[i].answerOne + "</button></li><li><button class='answer-choice' type='button'>2. " + questions[i].answerTwo + "</button></li><li><button class='answer-choice' type='button'>3. " + questions[i].answerThree + "</button></li><li><button class='answer-choice' type='button'>3. " + questions[i].answerFour + "</button></li>";
+for (var i = 0; i < questions.length; i++) {
+    quizQuestion.innerHTML = "<h1>" + questions[i].question + "</h1>";
+    
+    quizQuestionContainer.innerHTML = "<li><button class='answer-choice' type='button'>1. " + questions[i].answerOne + "</button></li><li><button class='answer-choice' type='button'>2. " + questions[i].answerTwo + "</button></li><li><button class='answer-choice' type='button'>3. " + questions[i].answerThree + "</button></li><li><button class='answer-choice' type='button'>3. " + questions[i].answerFour + "</button></li>";
 
-quizQuestion.innerHTML = "<h1>" + questions[i].question + "</h1>";
-
-quizQuestion.appendChild(quizQuestionContainer);
-mainEl.replaceChild(quizQuestion,startPage);
+    //debugger;
+    quizQuestion.appendChild(quizQuestionContainer);
+    if (document.querySelector(".start-page") === startPage) {
+        mainEl.replaceChild(quizQuestion,startPage);
+    }
+    else if (document.querySelector(".start-page" === !startPage)) {
+        mainEl.replaceChild(quizQuestion,quizQuestion);
+    }
 }
+
+//quizQuestion.appendChild(quizQuestionContainer);
+//mainEl.replaceChild(quizQuestion,startPage); //This will probably only work for the first question, you need a variable for the child of mainEl to reference here, this will involve some re-factoring - I don't think this will address your issue with why the start-quiz jumps to the last question in the array, think more on it anyway, but this is still an important thing to fix. The for-loop should replace whatever the child of ``main'' is with the first question in the array, then the next. As the code stands, the function cannot replace the last question with the next question, because it's looking for the startpage object, which would have been replaced. Still, why does it jump to the last object? I think that this has something to do with the for loop itself - the former problem doesn't arise until you reach the replace method later. So something about the for-loop is automatically cycling through all the elements until you get to the last one. Maybe the answer is in the modules. Maybe not. 
 
 //mainEl = document.querySelector("#main");
 //mainEl.removeChild(startPage);
